58b3a469e57ed65a52b4d8953c6d8309c6bd2d61
mohamadjavadeng <mohamadjavadarab92@gmail.com>
2023-08-20 11:45:14 +0330

    first git tutorial V0.1

---
+++ testhamid.py
@@ -0,0 +1,17 @@
+Alist = [[1,2],[2,6],[3,7],[4,6],[9,8]]
+print(Alist)
+A1 = []
+A2 = []
+for i in Alist:
+    A1.append(i[0])
+    A2.append(i[1])
+print(A1)
+print(A2)
+
+for i in range(len(Alist)):
+    globals()['list%s' % i] = Alist[i]
+
+for i in range(len(Alist)):
+    print(globals()['list%s' % i])
+
+print(list1)
