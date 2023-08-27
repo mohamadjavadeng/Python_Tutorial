e892889b6cc8f151a0549e672056935542d4eb37
mohamadjavadeng <mohamadjavadarab92@gmail.com>
2023-08-27 11:04:00 +0330

    V1.1
    End of the tutorial

--- pyFunc.py
+++ pyFunc.py
@@ -66,3 +66,17 @@ def factorial(x):
 
 
 print(factorial(6))
+
+#---- Lambda
+x = lambda a, b : a * b
+print(x(5, 6))
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
