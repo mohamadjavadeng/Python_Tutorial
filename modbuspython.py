b620d1eabd9e7b52c2523919c68f66eef2a5d0ba
mohammadjavad arab <mohamadjavadarab92@gmail.com>
2024-07-10 14:39:13 +0400

    V2.0

--- modbuspython.py
+++ modbuspython.py
@@ -2,7 +2,7 @@ import serial
 import minimalmodbus
 from time import sleep
 
-client1 = minimalmodbus.Instrument('COM4', 1, debug=False)  # port name, slave address (in decimal)
+client1 = minimalmodbus.Instrument('COM13', 1, debug=False)  # port name, slave address (in decimal)
 client1.serial.baudrate = 9600  # baudrate
 client1.serial.bytesize = 8
 client1.serial.parity   = serial.PARITY_NONE
@@ -15,18 +15,19 @@ client1.clear_buffers_before_each_transaction = True
 sleep(2)
 print("program started")
 
-coil_value = client1.read_bit(1, functioncode=1)
+coil_value = client1.read_bit(1281, functioncode=1)
 
 # Print the value
 print(f"Value of coil 0: {coil_value}")
-client1.write_bit(1, not coil_value, functioncode=5)
+client1.write_bit(1281, not coil_value, functioncode=5)
 
-input_stats  = client1.read_register(0, 1, 3) # read single register 2bytes (16bit)
-print("input stats: {0:016b}".format(input_stats))
-output_stats  = client1.read_register(1, 1, 3) # read single register 2bytes (16bit)
-print("output stats: {0:016b}".format(output_stats))
-output_stats  = client1.read_register(2, 1, 3) # read single register 2bytes (16bit)
-print("output stats: {0:016b}".format(output_stats))
+input_stats  = client1.read_register(4096, 1, 3) # read single register 2bytes (16bit)
+print(f"input stats: {input_stats}")
+output_stats  = client1.read_register(4097, 1, 3) # read single register 2bytes (16bit)
+print("output stats: {}".format(output_stats))
+output_stats  = client1.read_register(4098, 1, 3) # read single register 2bytes (16bit)
+print("output stats: {}".format(output_stats))
+client1.write_register(4098, 500) # write single register
 
 
 client1.close_port_after_each_call = True
